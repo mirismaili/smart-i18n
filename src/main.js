d1206b7f039e903cd786fc25c7008d8a9ca82de8
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2021-05-15 23:55:39 +0430

    `smartTranslation` added
     `smartTranslation` added to translate words and phrases (with defined translation) even between larger phrases or sentences (without defined translation).

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@
  * @author {@link https://mirismaili.github.io S. Mahdi Mir-Ismaili}
  */
 export default class I18n {
+	sortedMessagesByLengthDesc = {}
+	
 	constructor(messages) {
 		this.messages = {}
 		
@@ -47,17 +49,27 @@ export default class I18n {
 				if ($numerical['0']) {
 					const zeroCode = $numerical['0'].charCodeAt(0)
 					for (let i = 1; i < 10; i++)
-						if (!$numerical[i]) $numerical[i] = String.fromCharCode(zeroCode + i) // $numerical[i] ??= ...
+						if (!$numerical[i])
+							$numerical[i] = String.fromCharCode(zeroCode + i) // $numerical[i] ??= ...
 				}
 			}
 			
 			// fill tMessages - part 3: Regular Translations:
 			// Note: Each part can override its prior parts!
+			
+			// noinspection JSMismatchedCollectionQueryUpdate
+			const sortedMessages = this.sortedMessagesByLengthDesc[lang] = []
 			for (const m in messages[lang]) {
-				if (m.startsWith('$')) continue  // No other directives defined here!
+				if (m.startsWith('$') /* No other directives defined here! */
+						|| !m)
+					continue
 				
+				sortedMessages.push(m)
 				tMessages[m] = messages[lang][m]
 			}
+			
+			// Make smart translation ready (this will work if none of above parts can translate):
+			sortedMessages.sort((a, b) => b.length - a.length)
 			
 			// Create Full-Translator:
 			this[lang] = (stringOrStrings, ...values) => {
@@ -65,7 +77,7 @@ export default class I18n {
 				
 				return strings.map((string, i) =>
 						this.partialTranslator(string, lang) +
-						this.partialTranslator(i in values ? values[i] : '', lang),
+						(i in values ? this.partialTranslator(values[i], lang) : ''),
 				).join('')
 			}
 			
@@ -77,11 +89,8 @@ export default class I18n {
 		}
 	}
 	
-	getTranslator(lang) {
-		return this[lang]
-	}
-	
 	partialTranslator(stringOrNumber, lang) {
+		// console.debug({stringOrNumber});
 		if (typeof stringOrNumber === 'number' || stringOrNumber instanceof Number) {
 			const number = stringOrNumber
 			
@@ -91,34 +100,92 @@ export default class I18n {
 			stringOrNumber = String(stringOrNumber)
 		}
 		
-		if (!(typeof stringOrNumber === 'string' || stringOrNumber instanceof String))
-			throw Error(
+		if (!(typeof stringOrNumber === 'string' || stringOrNumber instanceof String)) {
+			console.trace(
 					'Argument can only be string or number. Boolean, null, undefined, etc. is not acceptable!\n' +
 					`stringOrNumber: ${stringOrNumber}\n` +
 					`type: ${typeof stringOrNumber}\n` +
 					`object-type: ${Object.prototype.toString.call(stringOrNumber)}`,
 			)
+			return String(stringOrNumber)
+		}
 		
 		const string = stringOrNumber
-		const message = string.trim()
-		const a = string.indexOf(message)
-		const b = a + message.length
-		const leadingSpaces = string.slice(0, a)
-		const trailingSpaces = string.slice(b)
+		const {leadingSpaces, trimmedMessage, trailingSpaces} = purifyMessage(string)
+		// console.debug({trimmedMessage});
 		
-		const tMessage = this.messages[lang][message]
+		const tMessage = trimmedMessage && this.messages[lang][trimmedMessage]
 		
 		if (tMessage)
 			return leadingSpaces + tMessage + trailingSpaces
 		
 		// https://regex101.com/r/xnyZDa/2
-		const isPotentialNumber = /^(\d{1,3}(,\d{3})*|\d+)(\.\d+)?$/.test(message)
+		const isPotentialNumber = /^(\d{1,3}(,\d{3})*|\d+)(\.\d+)?$/.test(trimmedMessage)
+		// console.debug({isPotentialNumber});
 		
 		if (isPotentialNumber)
 			return leadingSpaces +
-					[...message].map(ch => this.messages[lang].$numerical[ch]).join('')
+					[...trimmedMessage].map(ch => this.messages[lang].$numerical[ch]).join('')
 					+ trailingSpaces
 		
-		return string
+		return this.smartTranslation(string, lang)
 	}
+	
+	smartTranslation(string, lang) {
+		const sortedMessages = this.sortedMessagesByLengthDesc[lang]
+		const tMessages = this.messages[lang]
+		return smartTranslationR(string)
+		
+		function smartTranslationR(string) {
+			for (const m of sortedMessages) {
+				const match = new RegExp(
+						'\\b' +
+						m.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') +  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+						'\\b',
+						'i',
+				).exec(string)
+				
+				if (match) {
+					const offset = match.index
+					
+					const beforeM = string.slice(0, offset)
+					const afterM = string.slice(offset + m.length)
+					let tBeforeM, tAfterM  // translated-...
+					{
+						const {leadingSpaces, trimmedMessage, trailingSpaces} = purifyMessage(beforeM)
+						tBeforeM = leadingSpaces + (trimmedMessage && smartTranslationR(trimmedMessage)) + trailingSpaces
+					}
+					{
+						const {leadingSpaces, trimmedMessage, trailingSpaces} = purifyMessage(afterM)
+						tAfterM = leadingSpaces + (trimmedMessage && smartTranslationR(trimmedMessage)) + trailingSpaces
+					}
+					
+					return tBeforeM + tMessages[m] + tAfterM
+				}
+			}
+			return string
+		}
+	}
+	
+	getTranslator(lang) {
+		return this[lang]
+	}
+}
+
+function purifyMessage(message) {
+	const trimmedMessage = message.trim()
+	const a = message.indexOf(trimmedMessage)
+	const b = a + trimmedMessage.length
+	const leadingSpaces = message.slice(0, a)
+	const trailingSpaces = message.slice(b)
+	return {leadingSpaces, trimmedMessage, trailingSpaces}
+}
+
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions#escaping
+ * @param string
+ * @returns {*}
+ */
+function escapeRegExp(string) {
+	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') // $& means the whole matched string
 }
--- test/I18n.test.js
+++ test/I18n.test.js
@@ -1,6 +1,6 @@
 import {strict as assert} from 'assert'
-import I18n from '../src/main.js'
 import messages from '../sample-data/messages.js'
+import I18n from '../src/main.js'
 
 /**
  * Created on 1400/2/16 (2021/5/6).
@@ -75,3 +75,21 @@ assert.equal(t`${0}`, '۰')
 assert.equal(t`2${3}`, '۲۳')
 assert.equal(t`4${5}6`, '۴۵۶')
 assert.equal(t`7${'8'}9`, '۷۸۹')
+
+assert.equal(t`:Hello`, ':سلام')
+assert.equal(t`world.`, 'دنیا.')
+assert.equal(t`/water!`, '/آب!')
+
+assert.equal(t`: Earth`, ': زمین')
+assert.equal(t`Sun .`, 'خورشید .')
+assert.equal(t`/ Moon !`, '/ ماه !')
+
+assert.equal(t` : star `, ' : ستاره ')
+assert.equal(t` the stars . `, ' ستارگان . ')
+assert.equal(t` / Human ! `, ' / انسان ! ')
+
+assert.equal(t`You aren't Venus!`, 'You aren\'t ناهید!')
+assert.equal(t`You are Venus or Mars!`, 'You are ناهید یا بهرام!')
+
+assert.equal(t`VenusMars!`, 'VenusMars!')
+assert.equal(t`Venus/Mars!`, 'ناهید/بهرام!')
